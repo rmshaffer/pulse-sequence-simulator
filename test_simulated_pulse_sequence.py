340c848f6828356e2a91156b9984a490dfee28c1
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2021-08-21 15:40:20 -0400

    Add sample MS gate to test script

--- test_simulated_pulse_sequence.py
+++ test_simulated_pulse_sequence.py
@@ -1,6 +1,13 @@
+import simulated_parameter_vault
 import simulated_pulse_sequence
 
-result = simulated_pulse_sequence.run_simulation(
+#
+# Single-ion Rabi flopping
+#
+simulated_parameter_vault.set_parameter(["IonsOnCamera", "ion_number"], 1)
+simulated_parameter_vault.set_parameter(["StateReadout", "readout_mode"], "pmt")
+
+rabi_result = simulated_pulse_sequence.run_simulation(
     "sequences/rabi_flopping.py",
     "RabiFlopping",
     {
@@ -14,4 +21,31 @@ result = simulated_pulse_sequence.run_simulation(
     },
 )
 
-print(result)
+#
+# Two-ion MS gate
+#
+simulated_parameter_vault.set_parameter(["IonsOnCamera", "ion_number"], 2)
+simulated_parameter_vault.set_parameter(["StateReadout", "readout_mode"], "camera_states")
+
+ms_result = simulated_pulse_sequence.run_simulation(
+    "sequences/molmer_sorensen.py",
+    "MolmerSorensenGate",
+    {
+        "MolmerSorensen-Scan_Selection": "MolmerSorensen.duration",
+        "MolmerSorensen:MolmerSorensen.duration": {
+            "ty": "RangeScan",
+            "start": 0,
+            "stop": 100e-6,
+            "npoints": 20
+        },
+    },
+)
+
+#
+# Print results
+#
+print("*** Single-ion Rabi flopping results ***")
+print(rabi_result)
+
+print("*** Two-ion MS gate results ***")
+print(ms_result)
